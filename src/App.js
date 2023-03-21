df0be5c57e0d95a55dbf80dbddd04dd8a7691d5b
saluvalasaichandu <saluvalasaichand@gmail.com>
2023-03-21 20:58:54 +0530

    SetStates_classFunction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import Message from './components/Message';
+import Counter from './components/Counter';
 class App extends Component{
   render(){
   return (
-
     <div>
-      <Message/>
+      <Counter/>
     </div>
     
 
