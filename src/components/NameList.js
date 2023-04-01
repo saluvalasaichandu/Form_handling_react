45b8c6ba8a2c7b6c566ab75b2bc41701693a5708
saluvalasaichandu <saluvalasaichand@gmail.com>
2023-04-01 19:11:59 +0530

    Index_Key

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -19,30 +19,11 @@ import React from 'react'
 import Person from './Person'
 
 function NameList() {
-  const names = ['Bruce', 'Clark', 'Diana', 'Bruce']
-  const persons = [
-    {
-      id: 1,
-      name: 'Bruce',
-      age: 30,
-      skill: 'React'
-    },
-    {
-      id: 2,
-      name: 'Clark',
-      age: 25,
-      skill: 'Angular'
-    },
-    {
-      id: 3,
-      name: 'Diana',
-      age: 28,
-      skill: 'Vue'
-    }
-  ]
-  const personList = persons.map(person => <Person key={person.id} person={person} />)
-  const nameList = names.map((name, index) => <h2 key={index}>{index} {name}</h2>)
-  return <div>{nameList}</div>
+  const names = ['abc','ijk','pqrs','xyz']
+
+  //const namelist=names.map(namee=><h1>{namee}</h1>)
+  const namelist=names.map((namee,index)=><h1 key={index}>index value is: {index} name is: {namee}</h1>)
+  return(<div>{namelist}</div>)
 }
 
 export default NameList
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,13 +1,9 @@
-import React from 'react'
+/*import React from 'react'
 
 function Person({person, key}) {
   return (
-    <div>
-      <h2>
-        I am {person.name}. I am {person.age} years old. I know {person.skill}
-      </h2>
-    </div>
+    
   )
 }
 
-export default Person
+export default Person*/
